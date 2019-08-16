7ab8ee507fffce6e616d9c20f7f9b380d75073a6
Lukas Olson <lukasdragon@live.ca>
2019-08-15 17:33:53 -0700

    Added some sfx, finished camera controller

--- Assets/Scripts/PlayerController/MouseCameraController.cs
+++ Assets/Scripts/PlayerController/MouseCameraController.cs
@@ -4,9 +4,15 @@ public class MouseCameraController : MonoBehaviour
 {
     [SerializeField] private float sensitivity = 5.0f;
     [SerializeField] private float smoothing = 2.0f;
+
+    [SerializeField] private float minRotation = -80f;
+    [SerializeField] private float maxRotation = 80f;
+    
     [SerializeField] private GameObject character;
+    private float _currentRotation;
     private Vector2 _mouseLook;
     private Vector2 _smoothV;
+    
 
 
     private void Start()
@@ -21,7 +27,8 @@ public class MouseCameraController : MonoBehaviour
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
         _smoothV.x = Mathf.Lerp(_smoothV.x, md.x, 1f / smoothing);
         _smoothV.y = Mathf.Lerp(_smoothV.y, md.y, 1f / smoothing);
-        _mouseLook += _smoothV;
+        _mouseLook.x += _smoothV.x;
+        _mouseLook.y = Mathf.Clamp(_mouseLook.y + _smoothV.y, minRotation, maxRotation);
 
         transform.localRotation = Quaternion.AngleAxis(-_mouseLook.y, Vector3.right);
         character.transform.localRotation = Quaternion.AngleAxis(_mouseLook.x, character.transform.up);
